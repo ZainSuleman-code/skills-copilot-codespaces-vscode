a5098468820383597db78b982bd266f3d06df129
Zain Suleman <159806894+ZainSuleman-code@users.noreply.github.com>
2026-05-10 21:35:25 +0000

    Copilot first commit

---
+++ skils.js
@@ -0,0 +1,16 @@
+function calculateNumbers(var1 , var2) {
+    var sum = var1 + var2;
+    var difference = var1 - var2;
+    var product = var1 * var2;
+    var quotient = var1 / var2;
+
+    return {
+        sum: sum,
+        difference: difference,
+        product: product,
+        quotient: quotient
+    };
+}
+
+var result = calculateNumbers(10, 5);
+console.log(result);
